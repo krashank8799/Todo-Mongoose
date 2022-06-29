1b974ddaad6446f3905722ad4e85b732f90db356
Krashank <68913696+krashank8799@users.noreply.github.com>
2022-06-29 20:50:07 +0530

    Update index.js

--- database/index.js
+++ database/index.js
@@ -1,6 +1,6 @@
 var mongoose = require("mongoose");
 
-const url = "mongodb+srv://Krashank:k12345678@cluster0.7moxc.mongodb.net/todoDb?retryWrites=true&w=majority"
+const url = "mongodb+srv://<YOUR USERNAME>:<YOUR PASSWORD>@cluster0.7moxc.mongodb.net/todoDb?retryWrites=true&w=majority"
 
 module.exports.start = function() {
     mongoose.connect(url).then(function() {
